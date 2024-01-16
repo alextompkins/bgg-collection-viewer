3c6658624a7fbbf05e029908ce174639b2d9ab53
James Wilkinson <james@jerw.ca>
2024-01-15 19:31:53 -0500

    Styles

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -18,27 +18,31 @@ const Game: React.FC<TGame> = ({
         <img
           src={thumbnail}
           alt={name}
-          className="object-cover object-top h-full w-full"
+          className="object-cover object-top h-full w-full rounded-t-lg"
         />
       </div>
 
       <div className="flex flex-col h-full p-4 leading-normal">
-        <h2 className="mb-2 text-2xl font-bold tracking-tight">{name}</h2>
-        <p>Year Published: {yearpublished}</p>
-        <p>Min Players: {minplayers}</p>
-        <p>Max Players: {maxplayers}</p>
-        <p>Min Playtime: {minplaytime} minutes</p>
-        <p>Max Playtime: {maxplaytime} minutes</p>
-        <p>Playing Time: {playingtime} minutes</p>
+        <h2 className="mb-2 text-2xl font-bold tracking-tight">
+          {name} ({yearpublished})
+        </h2>
+        <p>
+          Players: {minplayers} - {maxplayers}
+        </p>
+        <p>
+          Playtime: {minplaytime} - {maxplaytime} minutes
+        </p>
         <p>Number of Plays: {numplays}</p>
       </div>
-      <a
-        className="w-full mt-4 p-4 bg-blue-900 hover:bg-slate-900 rounded-lg"
-        href={`https://boardgamegeek.com/boardgame/${bggId}`}
-        target="_blank"
-      >
-        View on BGG
-      </a>
+      <div className="p-4 w-full flex">
+        <a
+          className=" text-center w-full mt-4 p-4 bg-blue-900 hover:bg-slate-900 rounded-lg"
+          href={`https://boardgamegeek.com/boardgame/${bggId}`}
+          target="_blank"
+        >
+          View on BGG
+        </a>
+      </div>
     </div>
   );
 };
