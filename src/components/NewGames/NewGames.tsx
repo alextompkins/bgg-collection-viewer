95b77f3a481ab6e89c843198352a2e7c9dff51a7
James Wilkinson <78602661+James-Wilkinson-git@users.noreply.github.com>
2024-08-18 10:23:31 -0400

    Vite issue with env

--- src/components/NewGames/NewGames.tsx
+++ src/components/NewGames/NewGames.tsx
@@ -21,7 +21,7 @@ const NewGames: React.FC = () => {
     const fetchGameDetails = async () => {
       try {
         const response = await fetch(
-          process.env.REACT_APP_API_URL || "http://localhost:4000/api/games"
+          import.meta.env.REACT_APP_API_URL || "http://localhost:4000/api/games"
         );
         const games: GameDetails[] = await response.json();
         setGameDetails(games);
