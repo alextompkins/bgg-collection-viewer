cd9005d363a9ac248e0ca75416c583b4ceab0c07
Alex Tompkins <alex@atomic.nz>
2025-08-31 13:18:30 +0200

    feat(functions): add caching to `get-collection`

--- netlify/functions/get-collection.ts
+++ netlify/functions/get-collection.ts
@@ -1,6 +1,7 @@
 // noinspection JSUnusedGlobalSymbols
 /* eslint-disable import/no-default-export */
 
+import { getStore } from '@netlify/blobs';
 import type { Config, Context } from '@netlify/functions';
 
 import { getCollection } from '../../src/api/bggApi';
@@ -10,12 +11,22 @@ export const config: Config = {
   path: '/api/collection/:collectionId',
 };
 
+const collectionStore = getStore('collections');
+
 async function getCollectionFn(_request: Request, context: Context): Promise<Response> {
   const { collectionId } = context.params;
-
   if (!collectionId) return new Response('collectionId param missing', { status: 400 });
 
-  const collection = await getCollection(collectionId);
+  const cachedCollection = await collectionStore.get(collectionId);
+  console.log('cachedCollection', JSON.parse(cachedCollection));
+
+  const collection = cachedCollection
+    ? JSON.parse(cachedCollection)
+    : await getCollection(collectionId);
+
+  if (!cachedCollection) {
+    collectionStore.setJSON(collectionId, collection).catch(console.error); // don't need to wait for this
+  }
 
   return new Response(JSON.stringify(collection), {
     status: 200,
